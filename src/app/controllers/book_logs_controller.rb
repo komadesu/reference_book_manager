edf9fd0ad27155fc4d367be0a86bb7df6d022ba1
komadesu <happykoma777@gmail.com>
2021-03-19 15:21:39 +0900

    feat: 登録ページのviewと機能を実装
    viewに関しては参考書データ登録用フォームがメインでform_withヘルパーを用いて
    book_logモデル作成用のフォームを作成した
    また作成がうまくいかなかった場合にはnewのviewを再度レンダリングさせるようにしたのと
    エラーメッセージをflashに保存してビューに渡すようにした
    これらに関してはまだモデルにおけるバリデーション処理を実装していないため未確認
    ただ理解のポイントとしてはこのRailsにおけるflashという機能は次のリクエストでのみ利用可能なデータで
    あり、flash.nowを用いることでまさにnewアクションをそのままのリクエストの中でrenderさせる様な時に
    最適である
    またこのflashはいわゆるリクエストスコープの様なものであると考えれば良い
    セッションスコープとリクエストスコープの使い分けをよく考えて実装する
    createアクションでBookLogモデル初期化用のパラメータはストロングパラメータ機能を利用して必要なデータ
    のみホワイトリストに載せ利用する様にした
    またそのためのメソッドはカプセル化させることがベストプラクティスとなっている

--- src/app/controllers/book_logs_controller.rb
+++ src/app/controllers/book_logs_controller.rb
@@ -3,9 +3,17 @@ class BookLogsController < ApplicationController
   end
 
   def new
+    @book_log = BookLog.new
   end
 
   def create
+    book_log = BookLog.new(book_log_params)
+    if book_log.save
+      redirect_to book_log
+    else
+      flash.now[:error] = 'データの登録に失敗しました'
+      render 'new'
+    end
   end
 
   def show
@@ -19,4 +27,11 @@ class BookLogsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_log_params
+    params.require(:book_log).permit(:title, :author, :status, :score, :summary)
+  end
+
 end
