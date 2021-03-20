e0766c854a81e1d5a7deda76365b8f9c56a00bc6
komadesu <happykoma777@gmail.com>
2021-03-20 15:28:54 +0900

    fix: データ保存処理のバグを修正
    データを作成する時にストロングパラメータを利用していて以前requireのせいでバグが
    出てたというのに少し違和感があったがそれはやはり必要だった
    なぜrequireでエラーが出る状況になってしまっていたかだが、これはバリデーションエラーの
    場合に登録ページを直接レンダリングするようになっていて、その場合@book_logというインスタンス変数
    にBookLogのモデルが入っていない状態になっていた。これはbook_logというローカル変数をcreateアクション内
    で使っていたからだ
    しかしビュー側ではform_withメソッドでフォームを作成していてその対象となるmodelに@book_logを使用していた
    newページを直接newアクションから呼び出す場合は@book_logを初期化していて渡していたが
    createアクションにおいてバリデーションが通らなかった場合はrenderで直接newビューをレンダリングするような
    コードになっていたため@book_logが見つからないという状況になっていた
    しかしRailsはそれで例外を投げるようにはなっておらず、ブラウザで実際に出来上がったHTMLをみるまで気づけなかった
    それぞれのフォームコントロールのname属性がストロングパラメータでrequire対象となるべきbook_logという
    ネームスペースがない状態になっていた
    そのため、一度バリデーションに引っかかった後にちゃんとした入力をしても、ストロングパラメータ内において
    require対象のbook_logは存在せずにもちろんデータの保存も実行されることなく永遠にバリデーションエラーが
    画面に出力される状況になっていたのである

--- src/app/controllers/book_logs_controller.rb
+++ src/app/controllers/book_logs_controller.rb
@@ -8,11 +8,11 @@ class BookLogsController < ApplicationController
   end
 
   def create
-    book_log = BookLog.new(book_log_params)
-    if book_log.save
-      redirect_to book_log, notice: 'データが正常に作成されました'
+    @book_log = BookLog.new(book_log_params)
+    if @book_log.save
+      redirect_to @book_log, notice: 'データが正常に作成されました'
     else
-      flash.now[:errors] = book_log.errors.full_messages
+      flash.now[:errors] = @book_log.errors.full_messages
       render 'new'
     end
   end
@@ -33,7 +33,7 @@ class BookLogsController < ApplicationController
   private
 
   def book_log_params
-    params.permit(:title, :author, :status, :score, :summary)
+    params.require(:book_log).permit(:title, :author, :status, :score, :summary)
   end
 
 end
